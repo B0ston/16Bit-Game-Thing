75cd357cd014bacb45654331770ac583cccb24da
B0ston <32821943+B0ston@users.noreply.github.com>
2018-06-06 18:48:21 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+
+	var width = canvas.width;
+	var height = canvas.height;
+
+	ctx.fillRect(0, 0, width, height);
+
+	function update() {
+		
+	}
+}
