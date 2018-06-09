dfaa62b132d0c5170210f001d998d027e0851abf
B0ston <32821943+B0ston@users.noreply.github.com>
2018-06-08 19:11:54 -0700

    added enemies, made animation function

---
+++ enemyTypes.js
@@ -0,0 +1,94 @@
+var EnemyTypes = {
+	"ENEMY_RAT": {
+		"models": {
+			"idleLeft": [
+				"assets/Rat/idleLeft/idleleft0.png",
+				"assets/Rat/idleLeft/idleleft1.png",
+				"assets/Rat/idleLeft/idleleft2.png",
+				"assets/Rat/idleLeft/idleleft3.png",
+				"assets/Rat/idleLeft/idleleft4.png"
+			],
+			"runningLeft": [
+				"assets/Rat/runningLeft/runningLeft0.gif",
+				"assets/Rat/runningLeft/runningLeft1.gif"
+			],
+			"attackingLeft": [
+				"assets/Rat/attackingLeft/attackingLeft0.png",
+				"assets/Rat/attackingLeft/attackingLeft1.png",
+				"assets/Rat/attackingLeft/attackingLeft2.png",
+				"assets/Rat/attackingLeft/attackingLeft3.png",
+				"assets/Rat/attackingLeft/attackingLeft4.png"
+			],
+
+			"attackingRight": [
+				"assets/Rat/attackingRight/attackingRight0.png",
+				"assets/Rat/attackingRight/attackingRight1.png",
+				"assets/Rat/attackingRight/attackingRight2.png",
+				"assets/Rat/attackingRight/attackingRight3.png",
+				"assets/Rat/attackingRight/attackingRight4.png"
+			],
+			"idleRight": [
+				"assets/Rat/idleRight/idleRight0.png",
+				"assets/Rat/idleRight/idleRight1.png",
+				"assets/Rat/idleRight/idleRight2.png",
+				"assets/Rat/idleRight/idleRight3.png",
+				"assets/Rat/idleRight/idleRight4.png"
+			],
+			"runningRight": [
+				"assets/Rat/runningRight/runningRight0.png",
+				"assets/Rat/runningRight/runningRight1.png"
+			]
+		},
+		"health": 50,
+		"armor": 0,
+		"canFly": false
+	},
+
+	"ENEMY_ROACH": {
+		"models": {
+			"idleLeft": [
+				"assets/Roach/idleLeft/idleLeft0.png",
+				"assets/Roach/idleLeft/idleLeft1.png",
+				"assets/Roach/idleLeft/idleLeft2.png",
+				"assets/Roach/idleLeft/idleLeft3.png",
+				"assets/Roach/idleLeft/idleLeft4.png",
+			],
+			"runningLeft": [
+				"assets/Roach/runningLeft/runningLeft0.png",
+				"assets/Roach/runningLeft/runningLeft1.png",
+				"assets/Roach/runningLeft/runningLeft2.png",
+				"assets/Roach/runningLeft/runningLeft3.png"
+			],
+			"attackingLeft": [
+				"assets/Roach/attackingLeft/attackingLeft0.png",
+				"assets/Roach/attackingLeft/attackingLeft1.png",
+				"assets/Roach/attackingLeft/attackingLeft2.png",
+				"assets/Roach/attackingLeft/attackingLeft3.png",
+				"assets/Roach/attackingLeft/attackingLeft4.png",
+			],
+			"idleRight": [
+				"assets/Roach/idleRight/idleRight0.png",
+				"assets/Roach/idleRight/idleRight1.png",
+				"assets/Roach/idleRight/idleRight2.png",
+				"assets/Roach/idleRight/idleRight3.png",
+				"assets/Roach/idleRight/idleRight4.png",
+			],
+			"runningRight": [
+				"assets/Roach/runningRight/runningRight0.png",
+				"assets/Roach/runningRight/runningRight1.png",
+				"assets/Roach/runningRight/runningRight2.png",
+				"assets/Roach/runningRight/runningRight3.png",
+			],
+			"attackingRight": [
+				"assets/Roach/attackingRight/attackingRight0.png",
+				"assets/Roach/attackingRight/attackingRight1.png",
+				"assets/Roach/attackingRight/attackingRight2.png",
+				"assets/Roach/attackingRight/attackingRight3.png",
+				"assets/Roach/attackingRight/attackingRight4.png",
+			]
+		},
+		"health": 25,
+		"armor": 15,
+		"canFly": false
+	}
+}
--- game.js
+++ game.js
@@ -5,9 +5,68 @@ $(document).ready(function() {
 	var width = canvas.width;
 	var height = canvas.height;
 
-	ctx.fillRect(0, 0, width, height);
+	var Enemies = [];
 
-	function update() {
-		
+	canvas.addEventListener('click', function(event) {
+		Enemies.push(new Enemy("ENEMY_RAT"))
+		Enemies[Enemies.length - 1].pos = {x: event.x, y:event.y};
+	})
+
+	ctx.imageSmoothingEnabled = false;
+
+	//Animation updater.
+	function updateAnims() {
+		ctx.clearRect(0, 0, width, height);
+		for (k in Enemies) {
+			Enemies[k].checkState();
+			Enemies[k].animateArray(Enemies[k].currentState);
+		}
 	}
-}
+
+	//Update function.
+	function update() {
+	}
+	setInterval(update, 10);
+	setInterval(updateAnims, 120);
+
+	//Enemy constructor.
+	function Enemy(t) {
+		this.properties = EnemyTypes[t];
+		this.pos = {x: 0, y:0};
+		this.currentState = this.properties.models.runningLeft;
+		this.previousState = this.currentState;
+
+		this.frames;
+		this.Images = [];
+		this.count = 0;
+
+		this.checkState = function() {
+			// console.log(this.currentState != this.previousState);
+			if (this.currentState != this.previousState) {
+				for (var i in this.currentState) {
+					this.Images[i] = new Image();
+					this.Images[i].src = this.currentState[i];
+				}
+
+				this.previousState = this.currentState;
+			}
+		}
+
+		for (var i in this.currentState) {
+			this.Images[i] = new Image();
+			this.Images[i].src = this.currentState[i];
+		}
+
+		this.animateArray = function() {
+			var self = this;
+			setTimeout(function() {
+				if (self.count > self.currentState.length - 1) {
+					self.count = 0;
+				}
+				ctx.drawImage(self.Images[self.count], self.pos.x, self.pos.y);
+				self.count++;
+			}, 120);
+		}
+	}
+
+})
